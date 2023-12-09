375302e5129afe359cc30b1962185cfd7c5bc2de
Rachel Blake <blakerach1@gmail.com>
2023-12-09 13:10:40 +0800

    show and hide relevant information based on logged status working

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from . import views
 
 app_name = 'news'
@@ -6,6 +7,6 @@ app_name = 'news'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
-    path('add-story/', views.AddStoryView.as_view(), name='newStory'),
+    path('add-story/', login_required(views.AddStoryView.as_view(), login_url='login'), name='newStory'),
     path('author/<username>/', views.AuthorStoryView.as_view(), name='authorStories'),
 ]
